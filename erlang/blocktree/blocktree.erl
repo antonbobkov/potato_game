73723f61fa66763d88eda541a57a19955056d2d8
texfan <texfan@gmail.com>
2018-08-03 23:28:18 +0000

    It compiles :terror:

---
+++ erlang/blocktree/blocktree.erl
@@ -0,0 +1,43 @@
+-module(blocktree).
+-export([add_new_transaction/2]).
+
+-record(transaction, {game_data, nonce, player_id, consensus_data}).
+
+add_new_transaction_to_array(TransactionArray, #transaction{nonce=Nonce}=Transaction) ->
+    TA = TransactionArray,
+    ArrSz = array:size(TransactionArray),
+    if
+	Nonce > ArrSz ->
+	    {TA, ignore_nonce_too_high};
+	Nonce < ArrSz ->
+	    ArrayTransaction = array:get(nonce),
+	    if 
+		ArrayTransaction == Transaction ->
+		    {TA, ignore_duplicate};
+		ArrayTransaction /= Transaction ->
+		    {TA, error_same_nonce_different_transaction}
+	    end;
+	Nonce == ArrSz ->
+	    {array:set(Nonce, Transaction, TA), added}
+    end.
+	    
+add_new_transaction_to_map(TransactionMap, #transaction{player_id=Id}=Transaction) ->
+    Result = maps:find(Id, TransactionMap),
+    case Result of
+	{ok, TransactionArray} ->
+	    Msg2 = existing_player;
+	error ->
+	    TransactionArray = array:new(),
+	    Msg2 = new_player
+    end,
+
+    {NewTA, Msg} = add_new_transaction_to_array(TransactionArray, Transaction),
+    NewTM = maps:put(Id, NewTA),
+    {NewTM, Msg, Msg2}.
+    
+add_new_transaction(VerifierData, Transaction) ->
+    {_, TransactionMap} = VerifierData,
+    add_new_transaction_to_map(TransactionMap, Transaction).
+
+	    
+    
