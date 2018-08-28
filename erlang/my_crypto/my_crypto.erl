acd467ffb34f197deecd70aa11573111fbdf7b4f
texfan <texfan@gmail.com>
2018-08-28 22:41:42 +0000

    boop: abstract cryptography we use into a module

---
+++ erlang/my_crypto/my_crypto.erl
@@ -0,0 +1,23 @@
+-module(my_crypto).
+
+-export([hash/1, sign/2, verify/3, read_file_key/2]).
+
+%% -include_lib("stdlib/include/assert.hrl").
+%% -include_lib("public_key/include/public_key.hrl"). 
+
+hash(Bin) -> crypto:hash(sha256, Bin).
+
+sign(Hash, Key) -> public_key:sign(Hash, none, Key).
+verify(Hash, Signature, PubKey) -> public_key:verify(Hash, none, Signature, PubKey).
+    
+
+read_file_key(private, FileName) ->
+    {ok, PemBin} = file:read_file(FileName),
+    [RSAEntry] = public_key:pem_decode(PemBin),
+    Key = public_key:pem_entry_decode(RSAEntry),
+    Key;
+
+read_file_key(public, FileName) ->
+    {ok, Bin} = file:read_file(FileName),
+    [{Key, _}] = public_key:ssh_decode(Bin, public_key),
+    Key.  
