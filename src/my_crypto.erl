aca03df9f64324d172270711dc9aeaccf99e118d
Peter Lu <chippermonky@gmail.com>
2018-09-16 13:24:59 -0700

    added types to my_crypto

--- src/my_crypto.erl
+++ src/my_crypto.erl
@@ -2,12 +2,16 @@
 
 -export([hash/1, sign/2, verify/3, read_file_key/2]).
 
+-spec hash(iodata()) -> binary().
 hash(Bin) -> crypto:hash(sha256, Bin).
 
-sign(_Hash, _PrivateKey) -> sign. %%public_key:sign(Hash, none, PrivateKey).
-verify(_Hash, _Signature, _PubKey) -> true. %%public_key:verify(Hash, none, Signature, PubKey).
-    
+-spec sign(binary(), public_key:private_key()) -> binary().
+sign(_Hash, _PrivateKey) -> <<>>. %%public_key:sign(Hash, none, PrivateKey).
 
+-spec verify(binary(), binary(), public_key:public_key()) -> boolean().
+verify(_Hash, _Signature, _PubKey) -> true. %%public_key:verify(Hash, none, Signature, PubKey).
+
+-spec read_file_key(private, string()) -> _.
 read_file_key(private, FileName) ->
     {ok, PemBin} = file:read_file(FileName),
     [RSAEntry] = public_key:pem_decode(PemBin),
