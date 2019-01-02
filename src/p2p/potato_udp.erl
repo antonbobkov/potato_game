f42a43f33f34ad537476d5c21e5d16a91f30f9b4
texfan <texfan@gmail.com>
2019-01-02 20:10:19 +0000

    boop: udp_dev

--- src/p2p/potato_udp.erl
+++ src/p2p/potato_udp.erl
@@ -6,43 +6,49 @@
 -type udp_ip() :: string().
 -type udp_port() :: integer().
 -type udp_address() :: {udp_ip(), udp_port()}.
+-type node_id() :: any().
+-type node_address() :: {udp_address(), node_id()}.
 
--type state() :: {gen_udp:socket(), map()}.
+%% maps node_id to Pid of the process
+-type node_map() :: map().
 
-%% game map maps game id to list of game instances
--type game_map() :: map().
+-type state() :: {gen_udp:socket(), node_map()}.
+
 
 start_link(Port) ->
   gen_server:start_link({local, potato_udp}, potato_udp, Port, []).
 
-%% state maps {game_id} to game PID
--spec init(udp_port()) -> {ok, {gen_udp:socket(), game_map()}}.
+-spec init(udp_port()) -> {ok, state()}.
+
 init(Port) ->
   %%register(potato_udp, self()),
-  io:format("gen_udp open on port: ~p~n", [Port]),
+
   {ok, Socket} = gen_udp:open(Port, [binary, {active,true}]),
-  %% add to group?
+  %% io:format("gen_udp open on port: ~p~n", [Port]),
+
   {ok, {Socket, maps:new()}}.
 
 %% Never used
-handle_call(_E, _From, S) ->
-  {noreply, S}.
+handle_call(E, From, S) ->
+  erlang:error(unexptected_handle_call, [E, From, S]).
 
 -spec handle_cast(Arg, state()) -> {noreply, state()} when
-  Arg :: {add_game, {typetato:game_id(), typetato:verifier_id()}, pid()}
-    | {remove_game, {typetato:game_id(), typetato:verifier_id()}}
-    | {send, udp_address(), binary()}.
-%% add a {game_id, VerifierId} => Pid to map
-handle_cast({add_game,{Key, VerId},Pid}, {Socket, Map}) ->
-  VerList = maps:get(Key, Map, []),
-  Map2 = maps:put(Key, [{VerId, Pid} | VerList], Map),
-  {noreply, {Socket, Map2}};
-%% remove {game_id, VerifierId} from map
-handle_cast({remove_game,{Key, VerId}}, {Socket, Map}) ->
-  VerList = maps:get(Key, Map, []),
-  VerList2 = lists:filter(fun({Id, _}) -> Id == VerId end, VerList),
-  Map2 = maps:put(Key, VerList2, Map),
-  {noreply, {Socket, Map2}};
+  Arg :: {add_node, node_id(), pid()}
+       %% | {remove_node, {typetato:node_id(), typetato:verifier_id()}}
+       | {send, node_address(), any()}
+       | {send, [node_address()], any()}.
+
+%% add a node_id => Pid to map
+handle_cast({add_node, NodeId, Pid}, {Socket, NodeMap}) ->
+  NodeMap2 = maps:put(NodeId, Pid, NodeMap),
+  {noreply, {Socket, NodeMap2}};
+
+%% remove {node_id, VerifierId} from map
+%% handle_cast({remove_node,{Key, VerId}}, {Socket, Map}) ->
+%%   VerList = maps:get(Key, Map, []),
+%%   VerList2 = lists:filter(fun({Id, _}) -> Id == VerId end, VerList),
+%%   Map2 = maps:put(Key, VerList2, Map),
+%%   {noreply, {Socket, Map2}};
 
 %% send a message to a remote host
 handle_cast({send, {Address, Port}, Msg}, S) ->
@@ -50,10 +56,10 @@ handle_cast({send, {Address, Port}, Msg}, S) ->
   gen_udp:send(Socket, Address, Port, Msg),
   {noreply, S}.
 
-unknown_game_error(Packet, GameId) ->
-  %% TODO maybe remove from set if message could not be passed on (i.e. game died)?
-  logger:alert("received packet ~p for unknown game id ~p~n",[Packet, GameId]),
-  ok.
+%% unknown_node_error(Packet, NodeId) ->
+%%   %% TODO maybe remove from set if message could not be passed on (i.e. node died)?
+%%   logger:alert("received packet ~p for unknown node id ~p~n",[Packet, NodeId]),
+%%   ok.
 
 handle_info({udp, _Socket, _IP, _InPortNo, Packet}, S) ->
   {_, Map} = S,
@@ -64,25 +70,25 @@ handle_info({udp, _Socket, _IP, _InPortNo, Packet}, S) ->
       {noreply, S};
     {ok, {NetId, Data}} ->
       case NetId of
-        {game_id, GameId} ->
-          case maps:find(GameId, Map) of
+        {node_id, NodeId} ->
+          case maps:find(NodeId, Map) of
             {ok, VerList} ->
               lists:foreach(fun({_, Pid}) -> Pid ! Data end, VerList);
             error ->
-              unknown_game_error(Packet, GameId)
+              unknown_node_error(Packet, NodeId)
           end;
-        {targeted, {GameId, VerifierId}} ->
-          case maps:find(GameId, Map) of
+        {targeted, {NodeId, VerifierId}} ->
+          case maps:find(NodeId, Map) of
             {ok, VerList} ->
               case lists:search(fun({VerId, _}) -> VerId == VerifierId end, VerList) of
                 {value, {_, Pid}} ->
                   Pid ! Data;
                 false ->
                   %% TODO log verified id as well
-                  unknown_game_error(Packet, GameId)
+                  unknown_node_error(Packet, NodeId)
               end;
             error ->
-              unknown_game_error(Packet, GameId)
+              unknown_node_error(Packet, NodeId)
           end
       end,
       {noreply, S}
