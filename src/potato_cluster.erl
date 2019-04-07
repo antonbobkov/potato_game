6b2aa8e0079fc74a9115e4c8e241b1c03f566ec2
anton_docker <texfan@gmail.com>
2019-04-07 00:59:47 +0000

    boop: ACTUALLY WORKING

--- src/potato_cluster.erl
+++ src/potato_cluster.erl
@@ -2,7 +2,7 @@
 
 -export([
 	 hi/0,
-	 start_one_pop_verifier/4,
+	 start_one_pop_verifier/5,
 	 start_single_server_cluster/5,
 	 start_cluster_from_json/2,
 	 stop_cluster/1,
@@ -14,11 +14,20 @@
 
 -include("potato_records.hrl").
 
+json_find(Key, Map) when is_atom(Key) ->
+    maps:find(atom_to_binary(Key, utf8), Map).
+
+json_get(Key, Map) when is_atom(Key) ->
+    maps:get(atom_to_binary(Key, utf8), Map).
+
+json_put(Key, Value, Map) when is_atom(Key) ->
+    maps:put(atom_to_binary(Key, utf8), Value, Map).
+
 hi() ->
     ?debugFmt("hi", []),
     ok.
 
-start_one_pop_verifier(VerifierArr, JsonConf, MyIndex, UdpServerId) ->
+start_one_pop_verifier(VerifierArr, JsonConf, MyIndex, UdpServerId, OnEventFn) ->
 
     MyAddress = (array:get(MyIndex, VerifierArr))#verifier_public_info.network_data,
 
@@ -28,10 +37,9 @@ start_one_pop_verifier(VerifierArr, JsonConf, MyIndex, UdpServerId) ->
 			gen_server:cast({global, UdpServerId}, {send, DestAddressList, {MyAddress, MsgId, Data} })
 		end,
 
-    EventFn = fun(_,_) -> ok end,
     ConfPrivateKey = default,
 
-    ConfigData = {MyIndex, NetSendFn, EventFn, ConfPrivateKey},
+    ConfigData = {MyIndex, NetSendFn, OnEventFn, ConfPrivateKey},
 
     gen_server:start_link({global, MyNodeNetId}, pop_verifier, {json_map, JsonConf, ConfigData}, []),
 
@@ -39,10 +47,10 @@ start_one_pop_verifier(VerifierArr, JsonConf, MyIndex, UdpServerId) ->
 
     MyNodeNetId.
 
-start_single_server_cluster(VerifierArr, JsonConf, ServerAddress, UdpServerId, ForwardFn) ->
+start_single_server_cluster(VerifierArr, JsonConf, ServerAddress, UdpServerId, OnEventFn) ->
     {_Ip, Port} = ServerAddress,
 
-    gen_server:start_link({global, UdpServerId}, potato_udp, {Port, ForwardFn}, []),
+    gen_server:start_link({global, UdpServerId}, potato_udp, {Port, OnEventFn}, []),
 
     VerList = lists:filter(
 		fun(VerData) ->
@@ -59,23 +67,72 @@ start_single_server_cluster(VerifierArr, JsonConf, ServerAddress, UdpServerId, F
 
     IdList = lists:map(
 		fun(VerIndex) ->
-			start_one_pop_verifier(VerifierArr, JsonConf, VerIndex, UdpServerId)
+			start_one_pop_verifier(VerifierArr, JsonConf, VerIndex, UdpServerId, OnEventFn)
 		end, 
 		IndexList),
     IdList.
 
-json_find(Key, Map) when is_atom(Key) ->
-    maps:find(atom_to_binary(Key, utf8), Map).
 
-json_get(Key, Map) when is_atom(Key) ->
-    maps:get(atom_to_binary(Key, utf8), Map).
+start_cluster_from_json(JsonConf_0, OnEventFn) ->
 
-format_message(Code, _Data) ->
-    io_lib:format("~p ~n", [Code]).
+    JsonConf = 
+	case json_get(genesis_block_timestamp_sec, JsonConf_0) of
+
+	    <<"now">> ->
+		Time = erlang:system_time(second),
+		json_put(genesis_block_timestamp_sec, Time, JsonConf_0);
+
+	    _ ->
+		JsonConf_0
+	end,
+
+    
+    
+    VerifierArr = pop_verifier:make_verifier_array_from_json(JsonConf),
+
+    VerAddressList = lists:map(
+		       fun(VerData) ->
+			       {VerAddress, _} = VerData#verifier_public_info.network_data,
+			       VerAddress
+		       end,
+		       array:to_list(VerifierArr)),
+
+    VerAddressUniqueList = sets:to_list(sets:from_list(VerAddressList)),
+
+    ProcessData = lists:map(
+		    fun(Address) ->
+			    UdpServerId = {udp_server, Address},
+			    IdList = start_single_server_cluster(VerifierArr, JsonConf, Address, UdpServerId, OnEventFn),
+			    {UdpServerId, IdList}
+		    end, 
+		    VerAddressUniqueList),
+
+    {UdpServerIdList, VerifierIdList} = lists:unzip(ProcessData),
+
+    {UdpServerIdList, lists:flatten(VerifierIdList)}.
+    
+stop_cluster({UdpServerIdList, VerifierIdList}) ->
+    lists:foreach(
+      fun(UdpId) ->
+	      gen_server:stop({global, UdpId})
+      end,
+      UdpServerIdList),
+
+    lists:foreach(
+      fun(Id) ->
+	      gen_server:stop({global, Id})
+      end,
+      VerifierIdList),
+
+    ok.
+
+
+format_message(Code, Data) ->
+    io_lib:format("~p ~n ~p ~n ~n", [Code, Data]).
 
 start_web_cluster([JsonFileName, LogModeStr]) ->
 
-    ?debugFmt("~p ~n ~p ~n", [JsonFileName, LogModeStr]),
+    %% ?debugFmt("~p ~n ~p ~n", [JsonFileName, LogModeStr]),
 
     LogMode = LogModeStr,
 
@@ -104,59 +161,20 @@ start_web_cluster([JsonFileName, LogModeStr]) ->
 	    no_web
     end,
 
-    ForwardFn = 
+    OnEventFn = 
 	case LogMode of 
 	    no_logs ->
 		fun(_, _) -> ok end;
 	    console_logs ->
 		fun(Code, Data) -> io:format(format_message(Code, Data)) end;
 	    file_logs ->
+		?assertNotEqual(LogFile, no_log_file),
+		file:write_file(LogFile, ""),
 		fun(Code, Data) -> 
-			?assertNotEqual(LogFile, no_log_file),
 			file:write_file(LogFile, format_message(Code, Data), [append])
 		end
 	end,		   
 
-    start_cluster_from_json(JsonConf, ForwardFn),
-
-    ok.
-
-start_cluster_from_json(JsonConf, ForwardFn) ->
-    
-    VerifierArr = pop_verifier:make_verifier_array_from_json(JsonConf),
-
-    VerAddressList = lists:map(
-		       fun(VerData) ->
-			       {VerAddress, _} = VerData#verifier_public_info.network_data,
-			       VerAddress
-		       end,
-		       array:to_list(VerifierArr)),
-
-    VerAddressUniqueList = sets:to_list(sets:from_list(VerAddressList)),
-
-    ProcessData = lists:map(
-		    fun(Address) ->
-			    UdpServerId = {udp_server, Address},
-			    IdList = start_single_server_cluster(VerifierArr, JsonConf, Address, UdpServerId, ForwardFn),
-			    {UdpServerId, IdList}
-		    end, 
-		    VerAddressUniqueList),
-
-    {UdpServerIdList, VerifierIdList} = lists:unzip(ProcessData),
-
-    {UdpServerIdList, lists:flatten(VerifierIdList)}.
-    
-stop_cluster({UdpServerIdList, VerifierIdList}) ->
-    lists:foreach(
-      fun(UdpId) ->
-	      gen_server:stop({global, UdpId})
-      end,
-      UdpServerIdList),
-
-    lists:foreach(
-      fun(Id) ->
-	      gen_server:stop({global, Id})
-      end,
-      VerifierIdList),
+    start_cluster_from_json(JsonConf, OnEventFn),
 
     ok.
--- web_files/start_single_test_node.sh
+++ web_files/start_single_test_node.sh
@@ -6,6 +6,6 @@ rebar3 compile &&
 
 cd web_files &&
 
-erl -pa ebin -pa jsx_ebin -noshell -s potato_cluster start_web_cluster ./www/single_verifier_localhost_config.json file_logs
+erl -pa main_ebin -pa jsx_ebin -noshell -s potato_cluster start_web_cluster ./www/single_verifier_localhost_config.json file_logs
 # erl -pa ebin -noshell -s potato_cluster hi
 
